065f902039abe145b6cba0bb7af3558c7c479bf6
yaelRedlich <yael054859@gmail.com>
2024-12-26 11:50:46 +0200

    create model handling_files_and_folders

--- classes/Repository.py
+++ classes/Repository.py
@@ -1,14 +1,23 @@
+import json
 from datetime import datetime
-from operator import index
 from datetime import datetime
+from os import listdir
+
 from classes.Commit import Commit
 from models.handling_files_and_folders import *
+import os
 class Repository :
+
+
     def __init__(self,repository_path,user_name):
        self.dict_commits = {}
-       self.repository_path=repository_path
-       self.user_name=user_name
-       self.count_commit=0
+       self.repository_path = repository_path
+       self.user_name = user_name
+       self.wit_path = os.path.join(self.repository_path, ".wit")
+       self.commits_path = os.path.join(self.wit_path, "commits")
+       self.staging_area_path = os.path.join(self.wit_path, "Staging Area")
+       self.commits_json_path = os.path.join(self.wit_path, "commits.json")
+       self._load_commits()
 
     def __str__(self):
         commits_str = "\n".join(
@@ -21,33 +30,131 @@ class Repository :
             f"  Commits:\n{commits_str if commits_str else '  No commits yet.'}"
         )
 
-    def add_commit(self,message):
-      current_time = datetime.now()
-      formatted_time = current_time.strftime("%Y-%m-%d%H:%M:%S")
-      c = Commit (formatted_time, self.user_name, message)
-      self.dict_commits[self.count_commit] = c
-      self.count_commit += 1
-      print(self)
+
+    def _load_commits(self):
+        if os.path.exists(self.commits_json_path):
+            try:
+                with open(self.commits_json_path, "r", encoding="utf-8") as json_file:
+                    existing_data = json.load(json_file)
+                if isinstance(existing_data, dict):
+                    self.count_commit = len(existing_data)
+                    self.dict_commits = existing_data
+            except Exception as e:
+                print(f"Error loading commits: {e}")
+
+
+    def append_changing_file (self):
+        list_change = []
+        last_commit = find_last_created_folder(self.commits_path)
+        path_last_commit = os.path.join(self.commits_path,last_commit)
+        print(os.path.getctime(path_last_commit))
+        for item in os.listdir(self.repository_path):
+            if item != ".wit" and is_file_modified_after(os.path.join(self.repository_path,item),path_last_commit):
+                list_change.append(item)
+        return list_change
+
+
+    def add_commit(self, message):
+        current_time = datetime.now()
+        formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+        new_commit = Commit(formatted_time, self.user_name, message)
+        self.dict_commits[self.count_commit] = new_commit
+        self.count_commit += 1
+        try:
+            if os.path.exists(self.commits_json_path):
+                with open(self.commits_json_path, "r", encoding="utf-8") as json_file:
+                    existing_data = json.load(json_file)
+            else:
+                existing_data = {}
+            existing_data[self.count_commit] = new_commit.to_dict()
+            with open(self.commits_json_path, "w", encoding="utf-8") as json_file:
+                json.dump(existing_data, json_file, indent=4, ensure_ascii=False)
+        except Exception as e:
+
+            print(f"Error updating JSON file: {e}")
+
+
     def wit_init (self):
         try:
          create_folder(".wit",self.repository_path)
-         new_path = os.path.join(self.repository_path, ".wit")
-         create_folder("Staging Area",new_path)
+         create_folder("Staging Area",self.wit_path)
+         create_folder("commits",self.wit_path)
+         create_file("commits.json",self.wit_path)
+         write_file(self.commits_json_path,"{}")
         except FileExistsError as e:
             print(e)
         except FileNotFoundError as e:
             print(e)
 
+
     def wit_add(self,file_name):
         try:
-            new_path = os.path.join(self.repository_path, ".wit")
-            new_path =  os.path.join(new_path, "Staging Area")
             source_path = os.path.join(self.repository_path,file_name)
-            create_file(file_name,new_path)
-            new_path = os.path.join(new_path, file_name)
-            copy_file(source_path,new_path)
+            create_file(file_name,self.staging_area_path)
+            new_path = os.path.join(self.staging_area_path, file_name)
+            copy_file(source_path,new_path)######check!!! self.staging_area_path
         except FileNotFoundError as  e:
             print(e)
+
+
+    def wit_commit(self, message):
+       message += "(" + str(self.count_commit) + ")"
+       self.add_commit(message)
+       path_new_folder = os.path.join(self.commits_path, message)
+       #  בדיקה האם commit הוא גירסה  הראשון אם כן ניצור תיקייה
+       # אחרת נעתיק את הגירסה הקודמת
+       if not os.listdir(self.commits_path) :
+           create_folder(message,self.commits_path)
+       else:
+         last_folder = find_last_created_folder(self.commits_path)
+         path_last_folder = os.path.join(self.commits_path, last_folder)
+         copy_folder(path_last_folder, path_new_folder)
+       copy_files_and_overwrite(self.staging_area_path,path_new_folder)
+       emptying_folder(self.staging_area_path)
+
+
+    def wit_log(self):
+        try:
+            with open(self.commits_json_path, "r", encoding="utf-8") as json_file:
+                all_commits = json.load(json_file)
+                for key, value in all_commits.items():
+                    print(json.dumps({key: value}, indent=4, ensure_ascii=False))
+                    print("\n")
+                return all_commits
+        except FileNotFoundError as e:
+            print(e)
+
+
+    def wit_status(self):
+        list_names_file_in_staging_area = []
+        if folder_is_empty(self.staging_area_path):
+            print("No changes added to commit")
+        else:
+            list_names_file_in_staging_area = read_names_all_files_in_folder(self.staging_area_path)
+        list_file_changing = self.append_changing_file()
+        list_file_changing = [item for item in list_file_changing if item not in list_names_file_in_staging_area]
+        return list_names_file_in_staging_area,list_file_changing
+
+
+    def wit_checkout(self,commit_id):
+        all_commit = {}
+        message_commit = ""
+        if os.path.exists(self.commits_json_path):
+            try:
+                with open(self.commits_json_path, "r", encoding="utf-8") as json_file:
+                    all_commit = json.load(json_file)
+            except Exception as e:
+                print(f"Error loading commits: {e}")
+        for key,value in all_commit.items():
+            if key == commit_id :
+                message_commit = value["message"]
+        if message_commit == "":
+            return "id not valid"
+        for item in listdir(self.commits_path):
+            if item == message_commit :
+                commit_path = os.path.join(self.commits_path,message_commit)
+                copy_folder_without_parm(commit_path,self.repository_path,".wit")
+                return
 
 
 
@@ -55,9 +162,14 @@ class Repository :
 repo=Repository(r"C:\Users\user1\Desktop\python\test","yaelRedlich")
 #repo.wit_init()
 repo.wit_add("index.html")
-repo.wit_add("index1.html")
+#repo.wit_add("index1.html")
+#repo.wit_commit("commit 1")
+#repo.wit_commit("commit 2")
+#repo.wit_log()
+#repo.add_commit("yael")
+#repo.add_commit("hadasa")
+#repo.add_commit("yael")
+#repo.append_changing_file()
 
-#repo.add_commit("hii")
-#repo.add_commit("ghkuky")
-#print(repo.add_commit("yael"))
-
+#print(repo.wit_status())
+#repo.wit_checkout("18")
--- models/handling_files_and_folders.py
+++ models/handling_files_and_folders.py
@@ -1,41 +1,122 @@
 import os
 import shutil
 from importlib.metadata import files
+from operator import truediv
 from os.path import exists
 
-def create_folder(folder_name,path):
-    if not exists(path):
-        raise FileNotFoundError ("path not found")
-    new_path=os.path.join(path,folder_name)
-    if os.path.isdir(new_path):
-        raise FileExistsError ("folder already exists")
-    os.mkdir(new_path)
 
-def create_file(file_name,path):
-    if not exists(path):
-        raise FileNotFoundError ("path not found")
-    new_path=os.path.join(path,file_name)
-    open(new_path,"w").close()
-
-#דורס את התיקיה במידה וקיימת כבר תיקייה בשם זהה
-
-def write_file (path,text) :
+def create_folder(folder_name, path):
     if not exists(path):
         raise FileNotFoundError("path not found")
-    with  open(path,"a") as file:
+    new_path = os.path.join(path, folder_name)
+    if os.path.isdir(new_path):
+        raise FileExistsError("folder already exists")
+    os.mkdir(new_path)
+
+
+def create_file(file_name, path):
+    if not exists(path):
+        raise FileNotFoundError("path not found")
+    new_path = os.path.join(path, file_name)
+    open(new_path, "w").close()
+
+
+#דורס את קובץ במידה וקיימת כבר קובץ בשם זהה
+
+def write_file(path, text):
+    if not exists(path):
+        raise FileNotFoundError("path not found")
+    with  open(path, "a") as file:
         file.write(text)
+
+
 #מוסיפה ולא דורסת
+
+
 def read_file(path):
     if not exists(path):
         raise FileNotFoundError("path not found")
-    with open(path,"r") as file:
-       return file.read()
-    #יתכן שלא נשתמש
-def copy_file(source_path,destination_path):
-    shutil.copyfile(source_path,destination_path)
+    with open(path, "r") as file:
+        return file.read()
+
+
+def copy_file(source_path, destination_path):
+    shutil.copyfile(source_path, destination_path)
+
+
+def copy_folder(source_path, destination_path):
+    shutil.copytree(source_path, destination_path)
+
+
+def copy_folder_without_parm(source_path, destination_path, parm):
+    for item in os.listdir(destination_path):
+        if item != parm:
+            path = os.path.join(destination_path, item)
+            os.remove(path)
+    for item in os.listdir(source_path):
+        path = os.path.join(source_path, item)
+        new_path = os.path.join(destination_path, item)
+        copy_file(path, new_path)
+
+
+def find_last_created_folder(directory):
+    folders = [f for f in os.listdir(directory) if os.path.isdir(os.path.join(directory, f))]
+    last_created_folder = max(folders, key=lambda folder: os.path.getctime(os.path.join(directory, folder)))
+    return last_created_folder
+
+
+def copy_files_and_overwrite(source_dir, destination_dir):
+    for item in os.listdir(source_dir):
+        source_item = os.path.join(source_dir, item)
+        destination_item = os.path.join(destination_dir, item)
+        if os.path.isdir(source_item):
+            if not os.path.exists(destination_item):
+                os.makedirs(destination_item)
+            copy_files_and_overwrite(source_item, destination_item)
+        else:
+            shutil.copy2(source_item, destination_item)
+
+
+def folder_is_empty(path):
+    if not os.listdir(path):
+        return True
+    return False
+
+
+def emptying_folder(path):
+    for item in os.listdir(path):
+        try:
+            current_file = os.path.join(path, item)
+            os.remove(current_file)
+        except FileNotFoundError as e:
+            print(e)
+
+
+def read_names_all_files_in_folder(path):
+    all_names = []
+    for item in os.listdir(path):
+        try:
+            all_names.append(item)
+        except FileNotFoundError as e:
+            print(e)
+    return all_names
+
+
+def is_file_modified_after(path1, path2):
+    date_path1 = os.path.getmtime(path1)
+    date_path2 = os.path.getmtime(path2)
+    if date_path1 > date_path2:
+        return True
+    else:
+        return False
+
 
 #create_file("index1.html",r"C:\Users\user1\Desktop\python\test\1")
 #create_folder("1",r"C:\Users\user1\Desktop\python\test")
 #write_file(r"C:\Users\user1\Desktop\python\test\1\index.html","<h1>hello world</h1>")
 #print(read_file(r"C:\Users\user1\Desktop\python\test\1\index.html"))
 #copy_file(r"C:\Users\user1\Desktop\python\test\1\index.html",r"C:\Users\user1\Desktop\python\test\1\index1.html")
+#copy_folder(r"C:\Users\user1\Desktop\python\test",r"C:\Users\user1\Desktop\python\test2")
+#print(read_names_all_files_in_folder(r"C:\Users\user1\Desktop\python\test\.wit\commits\commit 1"))
+# print(is_file_modified_after(r"C:\Users\user1\Desktop\python\test2",r"C:\Users\user1\Desktop\python\test"))
+#copy_folder_without_parm(r"C:\Users\user1\Desktop\python\source", r"C:\Users\user1\Desktop\python\dist", ".wit")
